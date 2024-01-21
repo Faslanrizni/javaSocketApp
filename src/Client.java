807cebb44a758e656a4e4153f33f2fc32f103b78
Faslan Rizni <Faslan2002Rizni@gmail.com>
2024-01-21 10:47:11 +0530

    client handle updated new

---
+++ src/Client.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client{
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",8000);
+
+          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.println(bufferedReader.readLine());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the name");
+            String clientName = scanner.nextLine();
+            printWriter.println(clientName);
+
+            String responseData =  bufferedReader.readLine();
+            System.out.println(responseData);
+
+            Thread thread  = new Thread(new MessageHandler(socket));
+            thread.start();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+class MessageHandler implements Runnable{
+
+    private Socket socket;
+
+    MessageHandler(Socket socket ){
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        try{
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message;
+
+            while((message =bufferedReader.readLine()) != null){
+                System.out.println(message);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
